df98db89338e335c3293fe8d6335ecc8a55d21a1
Chris Fournier <chris.m.fournier@gmail.com>
2009-11-12 00:05:55 +0000

    Added required ART libs, and a barebones Agent class.

---
+++ agent/src/agent/CCMPAgent.java
@@ -0,0 +1,119 @@
+package agent;
+import testbed.agent.Agent;
+
+/**
+ * 
+ */
+
+/**
+ * @author cfournie
+ *
+ */
+public class CCMPAgent extends Agent {
+
+	/**
+	 * 
+	 */
+	public CCMPAgent() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param paramFile
+	 */
+	public CCMPAgent(String paramFile) {
+		super(paramFile);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see testbed.agent.Agent#initializeAgent()
+	 */
+	@Override
+	public void initializeAgent() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see testbed.agent.Agent#prepareCertaintyReplies()
+	 */
+	@Override
+	public void prepareCertaintyReplies() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see testbed.agent.Agent#prepareCertaintyRequests()
+	 */
+	@Override
+	public void prepareCertaintyRequests() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see testbed.agent.Agent#prepareOpinionCreationOrders()
+	 */
+	@Override
+	public void prepareOpinionCreationOrders() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see testbed.agent.Agent#prepareOpinionProviderWeights()
+	 */
+	@Override
+	public void prepareOpinionProviderWeights() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see testbed.agent.Agent#prepareOpinionReplies()
+	 */
+	@Override
+	public void prepareOpinionReplies() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see testbed.agent.Agent#prepareOpinionRequests()
+	 */
+	@Override
+	public void prepareOpinionRequests() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see testbed.agent.Agent#prepareReputationAcceptsAndDeclines()
+	 */
+	@Override
+	public void prepareReputationAcceptsAndDeclines() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see testbed.agent.Agent#prepareReputationReplies()
+	 */
+	@Override
+	public void prepareReputationReplies() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see testbed.agent.Agent#prepareReputationRequests()
+	 */
+	@Override
+	public void prepareReputationRequests() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
