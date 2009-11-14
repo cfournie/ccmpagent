74bbb0aaf9283770e0f0c7cdffbe676c8dbbe434
Mike Lepard <mike.lepard@gmail.com>
2009-11-14 05:11:58 +0000

    first pass at interface layout

--- bayestrustlib/src/trust/TrustInterface.java
+++ bayestrustlib/src/trust/TrustInterface.java
@@ -2,6 +2,7 @@
  * 
  */
 package trust;
+import testbed.sim.Era;;
 
 /**
  * @author cfournie
@@ -9,4 +10,25 @@ package trust;
  */
 public interface TrustInterface {
 
+	void addAgent( String newAgent );
+	void removeAgent( String agent );
+	
+	void setOurEraCertainty(Era era);
+
+	//handle a reputation response from an Agent about another Agent.
+	void receiveAgentReputationUpdate( String fromAgent, String aboutAgent, Era era, double reputation );
+	//return the weight (trust) for the sim to use in appraisal calculation
+	double getReputationWeight( String agent, Era era );
+	
+	//Get our local trust value, for dtlibrary
+	double getTrustValue( String agent);
+	//how much do we think the other agent trusts us?
+	double getPerceiveTrustValue( String agent );
+	
+	void updateAgentTrustValue( String agent, Era era, double paintingValue, double appraisalValue, double certainty);
+
+	void agentDidNotAcceptReputationRequest( String agent, Era era );
+	void agentDidNotProvideReputation( String agent, Era era );
+	void agentDidNotProvideCertainty( String agent, Era era);
+	void agentDidNotProvideOpinion( String agent, Era era);
 }
