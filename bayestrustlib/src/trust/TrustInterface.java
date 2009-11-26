0a706e96c3fde3fd3ac46308743fc30e5bc464aa
Catalin Patulea <cat@vv.carleton.ca>
2009-11-26 04:14:06 +0000

    TrustInterface changes

--- bayestrustlib/src/trust/TrustInterface.java
+++ bayestrustlib/src/trust/TrustInterface.java
@@ -6,10 +6,9 @@ import trust.model.primitives.Context;
 import trust.model.primitives.Peer;
 
 public interface TrustInterface {
-	
 	public boolean addPeer(Peer py) throws DuplicatePeerException;
 	public void storeEncounter(Context ck, Peer py, int lb) throws LevelRangeException;
-	public void storeRecommendation(Context ck, Peer py, int lb) throws LevelRangeException;
+	public void storeRecommendation(Context ck, Peer pr, Peer py, int lb) throws LevelRangeException;
 	public int getOverallTrust(Context ck, Peer py);
 	public double getOverallTrustConfidence(Context ck, Peer py);
 }
